e95b3f89b54760383d0b2b4758fdff05b4dd34d5
jamesjlandry <james.j.landry@outlook.com>
2020-12-14 16:40:37 -0600

    the Calendar will show up on web in testing, but not on mobile when using the navigation drawer. Cannot get either to swipe in from left. will require further testing

--- frontend/App.js
+++ frontend/App.js
@@ -1,46 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import Calendar from './components/Calendar.js'
 import Login from './components/login.js';
 import Signup from './components/signup.js';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import 'react-native-gesture-handler'
 
-const Stack = createStackNavigator(
-  {
-    Home: Calendar,
-    Login: Login,
-    Signup: Signup,
-  }
-)
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+
 
 const Drawer = createDrawerNavigator()
 
-CalendarOptions = () => {
-  return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Appointments" component={Calendar}/>
-      <Drawer.Screen />
-    </Drawer.Navigator>
-  )
-}
+
+  
+ 
 
 
 export default function App() {
   return <NavigationContainer>{
-
+       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        
+          <Drawer.Navigator>
+          <Drawer.Screen name="Appointments" component={Calendar}/>
+          <Drawer.Screen name="Assignments" component={Calendar}/>
+          <Drawer.Screen name="Errands" component={Calendar}/>
+          <Drawer.Screen name="Meals" component={Calendar}/>
+          <Drawer.Screen name="Projects" component={Calendar}/>
+          <Drawer.Screen name="All" component={Calendar}/>
+        </Drawer.Navigator>
+         {/* <Calendar/> */}
+       </View>
+       
     }</NavigationContainer>;
 }
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- frontend/components/Calendar.js
+++ frontend/components/Calendar.js
@@ -6,24 +6,24 @@ import WeeklyCalendar from 'react-native-weekly-calendar';
 const Calendar = ({ navigation }) =>  {
 
     const sampleEvents = [
-        { 'start': '2020-03-23 09:00:00', 'duration': '00:20:00', 'note': 'Walk my dog' },
-        { 'start': '2020-03-24 14:00:00', 'duration': '01:00:00', 'note': 'Doctor\'s appointment' },
-        { 'start': '2020-03-25 08:00:00', 'duration': '00:30:00', 'note': 'Morning exercise' },
-        { 'start': '2020-03-25 14:00:00', 'duration': '02:00:00', 'note': 'Meeting with client' },
-        { 'start': '2020-03-25 19:00:00', 'duration': '01:00:00', 'note': 'Dinner with family' },
-        { 'start': '2020-03-26 09:30:00', 'duration': '01:00:00', 'note': 'Schedule 1' },
-        { 'start': '2020-03-26 11:00:00', 'duration': '02:00:00', 'note': 'Schedule 2' },
-        { 'start': '2020-03-26 15:00:00', 'duration': '01:30:00', 'note': 'Schedule 3' },
-        { 'start': '2020-03-26 18:00:00', 'duration': '02:00:00', 'note': 'Schedule 4' },
-        { 'start': '2020-03-26 22:00:00', 'duration': '01:00:00', 'note': 'Schedule 5' }
+        { 'start': '2020-12-15 09:00:00', 'duration': '00:20:00', 'note': 'Walk my dog' },
+        { 'start': '2020-12-24 14:00:00', 'duration': '01:00:00', 'note': 'Doctor\'s appointment' },
+        { 'start': '2020-12-25 08:00:00', 'duration': '00:30:00', 'note': 'Morning exercise' },
+        { 'start': '2020-12-25 14:00:00', 'duration': '02:00:00', 'note': 'Meeting with client' },
+        { 'start': '2020-12-25 19:00:00', 'duration': '01:00:00', 'note': 'Dinner with family' },
+        { 'start': '2020-12-26 09:30:00', 'duration': '01:00:00', 'note': 'Schedule 1' },
+        { 'start': '2020-12-26 11:00:00', 'duration': '02:00:00', 'note': 'Schedule 2' },
+        { 'start': '2020-12-26 15:00:00', 'duration': '01:30:00', 'note': 'Schedule 3' },
+        { 'start': '2020-12-26 18:00:00', 'duration': '02:00:00', 'note': 'Schedule 4' },
+        { 'start': '2020-12-26 22:00:00', 'duration': '01:00:00', 'note': 'Schedule 5' }
       ]
     
       return (
         <View style={styles.container}>
-          <Button
+          {/* <Button
             title='Log Out'
             onPress={() => navigation.navigate('Login')}
-          />
+          /> */}
           <WeeklyCalendar events={sampleEvents} style={{ height: 400 }} />
         </View>
       );
